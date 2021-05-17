1a41848b7ae9497eccb29d0a655378163f7b4a80
Wolfgang Geisler <pw18l004@technikum-wien.at>
2021-05-17 23:27:18 +0200

    new Co-authored-by: Daniel <danielmuxel@users.noreply.github.com>

---
+++ src/components/Day.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ day.dt }}</h1>
+    <div>{{day.temp.min}} <i class="min fas fa-temperature-low"></i></div>
+    <div>{{day.temp.max}} <i class="max fas fa-temperature-high"></i></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['day']
+}
+</script>}
+
+<style>
+.max {
+    color: red;
+}
+.min {
+  color: blue;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,89 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <div v-if="error">{{error}}</div>
-    <div v-if="weather">{{weather.current.weather[0].description}}</div>
+    <h1>Weather App</h1>
+    <div v-if="error">{{ error }}</div>
+    <ul v-if="weather">
+      <li>{{ "Location: " + weather.timezone }}</li>
+      <li>
+        {{ weather.current.weather[0].main }}
+        <img :src="iconUrl" />
+      </li>
+      <li>{{ iconUrl }}</li>
+      <li>Temp: {{ currentTemp }}</li>
+      <li>Hum: {{ currentHum  }}</li>
+    </ul>
+    <!-- Day Components -->
+    <div v-if="daily !== null && daily.length" :key="day.dt">
+      <Day :day="day" />
+    </div>
+    <!-- 
+    <div v-for="(day, index) in daily" :key="index">
+    </div>
+    -->
+    <ul>
+      <li>{{ counter }}</li>
+      <li>{{ counterTimesTwo }}</li>
+      <li><button @click="addCounter">Add Counter</button></li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import axios from 'axios'
+import Day from '@/components/Day'
+import axios from "axios";
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Day,
+  },
   data() {
     return {
       weather: null,
-      error: null
-    }
+      error: null,
+      counter: 0,
+    };
   },
 
   created() {
     const apiKey = process.env.VUE_APP_WEATHER_API_KEY;
     let lat = 48.210033;
     let lon = 16.363449;
-    let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?appid=${apiKey}&lat=${lat}&lon=${lon}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?appid=${apiKey}&lat=${lat}&lon=${lon}&units=metric`;
     console.log(apiUrl);
 
-    axios.get(apiUrl)
-    .then(response => {
-      this.weather = response.data;
-      console.log(this.weather);
-    })
-    .catch(e => {
-      this.error = e.message;
-    })
-  } 
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        this.weather = response.data;
+        console.log(this.weather);
+      })
+      .catch((e) => {
+        this.error = e.message;
+      });
+  },
+  computed: {
+    iconUrl() {
+      return `https://openweathermap.org/img/wn/${this.weather.current.weather[0].icon}@2x.png`
+    },
+    currentTemp() {
+      return `${Math.round(this.weather.current.temp)} °C`
+    },
+    currentHum() {
+      return `${this.weather.current.humidity} %`
+    },
+    counterTimesTwo() {
+      return this.counter * 2
+    },
+    daily() {
+      console.log(this.weather)
+      return this.weather.daily
+    }
+  },
+  methods: {
+    addCounter() {
+      this.counter++
+    }
+  }
 };
 </script>
